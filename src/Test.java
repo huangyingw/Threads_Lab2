1d55c5e148ff0f155cc843b4d2f8c69dc3f04a24
huangyingw@linux_programming <huangyingw@gmail.com>
2012-05-06 10:29:38 -0500

    n

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,32 @@
-public class Test {
+class Test {
 	public static void main(String[] args) {
-		ParentThread pa = new ParentThread(100);
-		pa.start();
+		ThreadB b = new ThreadB();
+		b.start();
+		System.out.println("b is start....");
+		synchronized (b)// 括号里的b是什么意思,起什么作用?
+		{
+			try {
+				System.out.println("Waiting for b to complete...");
+				b.wait();// 这一句是什么意思，究竟让谁wait?
+				System.out.println("Completed.Now back to main thread");
+			} catch (InterruptedException e) {
+			}
+		}
+		System.out.println("Total is :" + b.total);
+	}
+}
+
+class ThreadB extends Thread {
+	int total;
+
+	public void run() {
+		synchronized (this) {
+			System.out.println("ThreadB is running..");
+			for (int i = 0; i < 100; i++) {
+				total += i;
+			}
+			System.out.println("total is " + total);
+			notify();
+		}
 	}
 }
