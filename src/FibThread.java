63e0696b443dce717d7167bf582b987148f047c9
huangyingw@linux_programming <huangyingw@gmail.com>
2012-05-06 11:27:42 -0500

    n

--- src/FibThread.java
+++ src/FibThread.java
@@ -1,15 +1,21 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class FibThread extends Thread {
 	int num;
+	private List<Long> data;
 
 	FibThread(int num) {
 		this.num = num;
+		data = new ArrayList<Long>();
 	}
 
-	public long Fib() {
+	public void Fib() {
 		if (num == 1)
-			return 0;
+			data.add((long) 0);
 		if (num < 4 && num > 1)
-			return 1;
+			data.add((long) 1);
 		else {
 			long a = 1;
 			long b = 1;
@@ -17,12 +23,23 @@ public class FibThread extends Thread {
 				b = a + b;
 				a = b - a;
 				// System.out.println("a = " + a + "  b = " + b);
+				data.add((long) a);
 			}
-			return a;
+
 		}
 	}
 
+	public String toString() {
+		String result = "";
+		Iterator<Long> it = data.iterator();
+		while (it.hasNext()) {
+			result += it.next().toString() + ",";
+		}
+		return result;
+	}
+
 	public void run() {
-		System.out.println(Fib());
+		Fib();
+		System.out.println(this);
 	}
 }
