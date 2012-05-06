9769f71aa8af83df111d11387d2b77755a255630
huangyingw@linux_programming <huangyingw@gmail.com>
2012-05-06 12:20:09 -0500

    n

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,5 @@
 class Test {
-	public static void main(String[] args) {
-		// Thread fib = new FibThread(6);
-		// fib.start();
+	public static void main(String[] args) throws InterruptedException {
 		Thread th = new ParentThread(10);
 		th.start();
 	}
